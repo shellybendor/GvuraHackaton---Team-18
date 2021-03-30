e2c7cb92f374c44f2a88ffb7fd0059c7dbcd176c
Netzer E <netzere@gmail.com>
2021-03-30 14:10:37 +0300

    Adding "HOME" template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
-
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/home')
+def home():
+    user = {'username': 'Netzer'}
+    return render_template('home.html', title='Home', user=user)
