b0b6418f7154cca678b17839a36d61e1a282b1f0
Netzer E <netzere@gmail.com>
2021-03-31 12:37:28 +0300

    Hey! Sending everything in

--- app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from app import app
-
-@app.route('/')
-@app.route('/home')
-def home():
-    user = {'username': 'Netzer'}
-    return render_template('home.html', title='Home', user=user)
